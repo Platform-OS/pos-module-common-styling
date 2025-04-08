c9a353143efa79d18065cd7f4d0a8faf8dc7e13c
Łukasz Krysiewicz <zx@secondgate.pl>
2025-04-08 13:09:04 +0200

    Added module init partial

---
+++ modules/common-styling/public/assets/js/pos.js
@@ -0,0 +1,12 @@
+/*
+  builds the needed namespace for platformOS ready-made components
+*/
+
+
+
+/* namespace */
+if(typeof window.pos !== 'object'){
+  window.pos = {};
+  window.pos.modules = {};
+  window.pos.translations = {};
+}
