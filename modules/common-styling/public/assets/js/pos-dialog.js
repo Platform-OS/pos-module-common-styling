374ddedf1f8420a15b952c1999ee89e58703e0bf
Łukasz Krysiewicz <zx@secondgate.pl>
2025-08-28 14:50:19 +0200

    Layout improv

--- modules/common-styling/public/assets/js/pos-dialog.js
+++ modules/common-styling/public/assets/js/pos-dialog.js
@@ -18,13 +18,13 @@ window.pos.modules.dialog = function(userSettings){
   // ------------------------------------------------------------------------
   module.settings = {};
   // dialog container (dom node)
-  module.settings.container = userSettings.container || document.querySelector('.pos-dialog');
+  module.settings.container = userSettings.container;
   // modal trigger (dom node)
-  module.settings.trigger = userSettings.trigger || document.querySelector('.pos-dialog-trigger');
+  module.settings.trigger = userSettings.trigger;
   // id used to mark the module (string)
-  module.settings.id = userSettings.id || module.settings.container.id;
+  module.settings.id = userSettings.id || module.settings.trigger.dataset.dialogtarget;
   // close button (dom nodes)
-  module.settings.closeButtons = userSettings.closeButtons || module.settings.container.querySelectorAll('.pos-dialog-close');
+  module.settings.closeButtons = userSettings.closeButtons || module.settings.container?.querySelectorAll('.pos-dialog-close');
   // to enable debug mode (bool)
   module.settings.debug = (userSettings?.debug) ? userSettings.debug : false;
 
@@ -33,10 +33,28 @@ window.pos.modules.dialog = function(userSettings){
   // purpose:		initializes the component
   // ------------------------------------------------------------------------
   module.init = () => {
+    pos.modules.debug(module.settings.debug, module.settings.id, 'Initializing dialog', module.settings);
+
+    if(!module.settings.container){
+      console.error('Could not find dialog container with ID ' + module.settings.id + ' while it\'s trigger is present', module.settings.trigger);
+    }
+
     module.settings.trigger.addEventListener('click', event => {
       event.preventDefault();
 
       module.open();
+    });
+
+    module.settings.container?.addEventListener('toggle', event => {
+      if(event.newState === 'open'){
+        pos.modules.debug(module.settings.debug, module.settings.id, 'Dialog opened', module.settings.container);
+        document.dispatchEvent(new CustomEvent('pos-dialog-opened', { bubbles: true, detail: { target: module.settings.container, id: module.settings.id } }));
+        pos.modules.debug(module.settings.debug, 'event', 'pos-dialog-opened', { target: module.settings.container, id: module.settings.id });
+      } else if(event.newState === 'closed') {
+        pos.modules.debug(module.settings.debug, module.settings.id, 'Dialog closed', module.settings.container);
+        document.dispatchEvent(new CustomEvent('pos-dialog-closed', { bubbles: true, detail: { target: module.settings.container, id: module.settings.id } }));
+        pos.modules.debug(module.settings.debug, 'event', 'pos-dialog-closed', { target: module.settings.container, id: module.settings.id });
+      }
     });
   };
 
